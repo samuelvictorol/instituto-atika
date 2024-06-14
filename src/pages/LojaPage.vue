1deed37d2f831fafac3173dc4bef9ec6a9ca9d5a
samuelvictorol <990905>
2024-06-14 17:30:52 -0300

    loja page

--- src/pages/LojaPage.vue
+++ src/pages/LojaPage.vue
@@ -1,6 +1,18 @@
 <template>
-    <q-page class="bg-accent">
-        <p>Loja</p>
+    <q-page class="relative bg-accent column items-center">
+        <div class="text-h4 q-py-md text-primary text-center">Loja</div>
+        <p class="text-center text-bold text-info">🚧 Loja em construção 🏗️<br>Em breve nossos produtos estarão disponíveis🛠️</p>
+        <img class="w80" src="~assets/drawn1.svg" alt="">
+        <div class="absolute-bottom w100 bg-accent q-py-md row justify-center ">
+            <a target="_blank" href="https://www.instagram.com/institutoatika/">
+                <img
+                class="rounded-borders"
+                  style="width:140px"
+                  alt="instagram logo"
+                  src="~assets/insta.png"
+                >
+              </a>
+          </div>
     </q-page>
 </template>
 
