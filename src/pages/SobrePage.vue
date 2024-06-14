b26f5a68d7598d4007cfd78ac9b1eeda7c87cfad
samuelvictorol <990905>
2024-06-14 17:00:58 -0300

    sobre page

--- src/pages/SobrePage.vue
+++ src/pages/SobrePage.vue
@@ -1,13 +1,80 @@
 <template>
-    <div>
-        <h1>Sobre</h1>
-    </div>
+    <q-page class="bg-accent">
+        <div class="w100 row justify-center ">
+            <img
+            class="q-pt-lg"
+              alt="Quasar logo"
+              src="~assets/sobreimg.png"
+            >
+          </div>
+          <p class="q-px-md text-card text-center q-py-md text-bold">Contribuir para o
+            desenvolvimento
+            socioeducativo da cidade de
+            Valparaiso de Goiás-GO,
+            utilizando práticas
+            educacionais, esportivas,
+            culturais e de saúde.</p>
+        <div class="w100 row justify-center ">
+            <img
+            class="q-pt-md"
+              alt="Quasar logo"
+              src="~assets/img3.png"
+            >
+        </div>
+        <div class="w100 row justify-center ">
+            <img
+              alt="Quasar logo"
+              src="~assets/img4.png"
+            >
+        </div>
+        <div class="text-h4 text-center text-primary q-pt-md">Doação 💸💖</div>
+        <div class="w100 q-mt-md row justify-center ">
+            <img
+              alt="Quasar logo"
+              src="~assets/img5.png"
+            >
+        </div>
+        <div class="row">
+            <q-btn label="Conheça nossa Loja e ainda ajude o projeto!" color="primary" icon="store" class="q-pa-md" @click="goTo('/loja')"/>
+        </div>
+        <div class="w100 row justify-center items-center q-mt-md">
+          <img src="~assets/insta.svg" style="width:30px" alt="">
+        <span class="text-h6 text-primary q-pl-md">Acompanhe-nos no Instagram!  </span>
+        </div>
+        <div class="w100 row justify-center ">
+            <img
+            class="q-pt-md"
+              alt="Quasar logo"
+              src="~assets/img2.png"
+            >
+          </div>
+          <div class="w100 bg-accent q-py-md row justify-center ">
+            <a href="">
+                <img
+                class="rounded-borders"
+                  style="width:140px"
+                  alt="instagram logo"
+                  src="~assets/insta.png"
+                >
+              </a>
+          </div>
+    </q-page>
 </template>
 
-<script>
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+function goTo(to) {
+    router.push(to)
+}
 
 </script>
 
-<style>
-
+<style scoped>
+.text-card {
+    font-size: 16px;
+    color: #806B64;
+    background: #E8D4A2;
+}  
 </style>
